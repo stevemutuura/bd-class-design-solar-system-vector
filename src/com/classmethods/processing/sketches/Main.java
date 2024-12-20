fc006f12f1bd65cfda7e1deda4e2ab7a0f9918cc
stevemutuura <stevemurimi44@gmail.com>
2024-12-20 06:08:30 +0300

    Completed Project

--- src/com/classmethods/processing/sketches/Main.java
+++ src/com/classmethods/processing/sketches/Main.java
@@ -1,11 +1,16 @@
 package com.classmethods.processing.sketches;
 
-import math.Vector;
+import  math.Vector;
 import processing.core.PApplet;
 
 public class Main extends PApplet {
     public static PApplet applet;
     StarDrawer starDrawer;
+
+    private Planet sun;
+    private Planet earth;
+    private Planet mercury;
+    private Planet mars;
 
     public void settings() {
         applet = this;
@@ -17,12 +22,35 @@ public class Main extends PApplet {
 
         colorMode(HSB);
         noStroke();
+
+        sun = new Planet(100, 50,38,
+                new Vector(300, 300), new Vector(0, 0), null);
+
+        earth = new Planet(0.8f , 11, 160,
+                new Vector(120, 300), new Vector(0, -2), sun);
+
+        mercury = new Planet(0.6f, 10, 20,
+                new Vector(200, 300), new Vector(0, -2.5f), sun);
+
+        mars = new Planet(1, 6, 0,
+                new Vector(200, 300), new Vector(0, -2.55f), sun);
     }
 
     public void draw() {
         fill(0,0,0,20);
         rect(0,0,width,height);
         starDrawer.drawStars();
+
+        sun.drawSelf();
+
+        earth.drawSelf();
+        earth.move();
+
+        mercury.drawSelf();
+        mercury.move();
+
+        mars.drawSelf();
+        mars.move();
 
     }
 
